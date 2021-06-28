5d3d6e1990ec6698e60b5723c38e37c9f3a89747
Ayanava Dutta <ayanava1999@gmail.com>
2021-06-29 00:06:53 +0530

    Update App.py

--- Webapp/App.py
+++ Webapp/App.py
@@ -1,3 +1,9 @@
+'''
+!/usr/bin/env tf
+v 0.1.0
+@author:ayanava_dutta
+-*-coding:utf-8-*-
+'''
 import streamlit as st 
 import pandas as pandas
 import numpy as np
@@ -7,7 +13,6 @@ from keras.models import Model
 from keras.models import load_model
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
-#from keras.preprocessing.image import load_img
 import matplotlib.cm as cm
 import keras
 from IPython.display import Image, display
@@ -15,8 +20,7 @@ import warnings
 from io import BytesIO
 from PIL import Image, ImageOps
 st.set_option('deprecation.showPyplotGlobalUse', False)
-import cv2
-from keras.models import model_from_json
+
 
 def main():
     st.title("Covid-19 Detection using Radiography images")
@@ -59,36 +63,30 @@ def get_img_array(img_path):
     return img
 
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
+ 
     grad_model = tf.keras.models.Model(
         [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
     )
 
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
+   
     with tf.GradientTape() as tape:
         last_conv_layer_output, preds = grad_model(img_array)
         if pred_index is None:
             pred_index = tf.argmax(preds[0])
         class_channel = preds[:, pred_index]
 
-    # This is the gradient of the output neuron (top predicted or chosen)
-    # with regard to the output feature map of the last conv layer
+   
     grads = tape.gradient(class_channel, last_conv_layer_output)
 
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
+    
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-    # We multiply each channel in the feature map array
-    # by "how important this channel is" with regard to the top predicted class
-    # then sum all the channels to obtain the heatmap class activation
+    
     last_conv_layer_output = last_conv_layer_output[0]
     heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
     heatmap = tf.squeeze(heatmap)
 
-    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy()
 
@@ -98,7 +96,7 @@ def save_and_display_gradcam(img_path , heatmap, cam_path="cam.jpg", alpha=0.4):
     img = keras.preprocessing.image.load_img(img_path)
     img = keras.preprocessing.image.img_to_array(img)
     heatmap = np.uint8(255 * heatmap)
-    jet = cm.get_cmap("inferno_r")
+    jet = cm.get_cmap("jet")
     jet_colors = jet(np.arange(256))[:, :3]
     jet_heatmap = jet_colors[heatmap]
     jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
